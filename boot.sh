d2da882928bc32fe69ea37d1d348fa27e84e558d
richard <whoami@hotmail.com>
2025-08-22 11:12:12 +0800

    debug

--- boot.sh
+++ boot.sh
@@ -3,9 +3,14 @@
 MODULE="${MODULE_NAME:-http_mcp}"
 echo "start module: ${MODULE}"
 echo "current dir `pwd`"
-#nohup /opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 http_service:app > server.log &2>&1 &
-# just for simplicity to deploy and share memory
-#/opt/llm_py_env/bin/gunicorn --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
+echo "start server..."
+/opt/mcp_py_env/bin/python server.py &
+sleep 3
+# 检查server.py是否仍在运行
+if ! ps -p $! > /dev/null; then
+    echo "Error: Server process failed to start!" >&2
+    exit 1
+fi
+echo "Server started successfully with PID: $!"
+echo "start http client"
 /opt/mcp_py_env/bin/gunicorn --certfile ./cert/srv.crt --keyfile ./cert/srv.key --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
-# real production environment
-#/opt/llm_py_env/bin/gunicorn --timeout 240 --preload -w 4 -b 0.0.0.0:19000 ${MODULE}:app
