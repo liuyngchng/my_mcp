388f1e8653e3fa723116985fa6f91f7238c9c8bd
richard <whoami@hotmail.com>
2025-08-26 11:04:14 +0800

    debug

--- sh/list_tools.sh
+++ sh/list_tools.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 
-curl -X POST -s http://localhost:8001/mcp   -H "Content-Type: application/json"   -H "Accept: application/json, text/event-stream"   -H "mcp-session-id: your_session_id"   -d '{
+curl --noproxy '*' -X POST -s http://localhost:19001/mcp   -H "Content-Type: application/json"   -H "Accept: application/json, text/event-stream"   -H "mcp-session-id: your_session_id"   -d '{
+    "jsonrpc": "2.0",
+    "id": 2,
+    "method": "tools/list"
+  }' | jq
+
+
+curl --noproxy '*' -X POST -s http://11.10.36.2:19006/mcp   -H "Content-Type: application/json"   -H "Accept: application/json, text/event-stream"   -H "mcp-session-id: your_session_id"   -d '{
     "jsonrpc": "2.0",
     "id": 2,
     "method": "tools/list"
