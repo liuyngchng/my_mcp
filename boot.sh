12cbb8a74a800d7af7e22d0d5dce34ef4ebef0e0
richard <whoami@hotmail.com>
2025-08-27 15:11:03 +0800

    debug

--- boot.sh
+++ boot.sh
@@ -1,10 +1,10 @@
 /bin/bash -c 'source ../llm_py_env/bin/activate'
 # for start application in docker container
 MODULE="${MODULE_NAME:-http_mcp}"
-echo "start module: ${MODULE}"
-echo "current dir `pwd`"
-echo "start server..."
-/opt/llm_py_env/bin/python server.py &
+MCP_SERVER='server.py'
+
+echo "start ${MCP_SERVER} ..."
+/opt/llm_py_env/bin/python ${MCP_SERVER} &
 sleep 3
 # 检查server.py是否仍在运行
 if ! ps -p $! > /dev/null; then
@@ -12,5 +12,6 @@ if ! ps -p $! > /dev/null; then
     exit 1
 fi
 echo "Server started successfully with PID: $!"
-echo "start http client"
+echo "start module: ${MODULE}"
+echo "current dir `pwd`"
 /opt/llm_py_env/bin/gunicorn --certfile ./cert/srv.crt --keyfile ./cert/srv.key --timeout 240 -w 1 --threads 8 -b 0.0.0.0:19000 ${MODULE}:app
